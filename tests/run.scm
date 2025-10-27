31cad381786174eda97fc3b20f212dedf5f9b8b4
Ivan Raikov <ivan.g.raikov@gmail.com>
2025-10-27 07:22:58 -0700

    added run script for all tests

---
+++ tests/run.scm
@@ -0,0 +1,7 @@
+(import scheme)
+
+(include-relative "autograd.scm")
+(include-relative "layer.scm")
+(include-relative "optimizer.scm")
+(include-relative "network.scm")
+(include-relative "serialization.scm")
